1e06f7c15004d46469534dd4e519dda234f069df
NoxiaLupus <noxia.lupus@gmail.com>
2021-12-04 16:48:00 +0100

    Adding the function to calculate after the new requirements

--- src/day2/MovingSubmarine.java
+++ src/day2/MovingSubmarine.java
@@ -33,4 +33,32 @@ public class MovingSubmarine {
         result = depthValue * horizontalValue;
         return result;
     }
+
+    /**
+     * This method calculates how far the submarine moves based on commands in consideration of the depth and horizontal
+     * position and the aim. After that it multiplies the depth and horizontal positions.
+     * @param commands is an ArrayList with a String Array, contains commands and values
+     * @return int which is the result of multiplying horizontal and depth values
+     */
+    public int multiplyCommandsWithAim(List<String[]> commands) {
+        int horizontalValue = 0, depthValue = 0, aimValue = 0, result;
+        for (String[] command : commands) {
+            String direction = command[0];
+            int value = Integer.parseInt(command[1]);
+            switch (direction) {
+                case "forward":
+                    horizontalValue = horizontalValue + value;
+                    depthValue = aimValue * value + depthValue;
+                    break;
+                case "down":
+                    aimValue = aimValue + value;
+                    break;
+                case "up":
+                    aimValue = aimValue - value;
+                    break;
+            }
+        }
+        result = depthValue * horizontalValue;
+        return result;
+    }
 }
